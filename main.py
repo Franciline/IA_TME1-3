1928c07a2b9e3510dd3e10b29602f524ddfb48dd
BshKatrin <dyanko.ekaterina2015@gmail.com>
2025-01-27 20:33:14 +0100

    main update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import exemple  # Pour pouvoir utiliser les methodes de exemple.py
 # Q1
-from gale_shapley import gale_shapley_etud, gale_shapley_parc
+from gale_shapley import gale_shapley_etud, gale_shapley_parc, get_instable
 from matrice_pref import matriceCE, matriceCP, genMatriceCE, genMatriceCP, genCapacite
 
 
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     cap, matCParc = matriceCP("PrefSpe.txt")
 
     optim_etud = gale_shapley_etud(matCE, matCParc, cap)
-    optim_parc = gale_shapley_parc(matCEtu, matCParc, cap)
+    optim_parc = gale_shapley_parc(matCE, matCParc, cap)
     print("Etudiant optimal : ", optim_etud)
     print("Parcours optimal : ", optim_parc)
-    print("Paires instables :", get_instable(matCEtu, matCParc, optim_etud))
+    print("Paires instables :", get_instable(matCE, matCParc, optim_etud))
     print("Matrice CE aléatoire :", genMatriceCE(5))
     print("Matrice CP aléatoire :", genMatriceCP(5))
